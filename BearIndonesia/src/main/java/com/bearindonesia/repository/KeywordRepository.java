094752444934f2f1ff42986435b16571f2a6d128
owonye <wyw24@sookmyung.ac.kr>
2026-01-28 05:50:34 +0900

    refactor: JPA 도메인/레포 정리
    - Article/Keyword 엔티티 제거 (JPA 저장 구조 삭제)
    - KeywordRepository 제거 (미사용 레포 정리)
    - DB 조회 기반 API 구조로 단순화

--- BearIndonesia/src/main/java/com/bearindonesia/repository/KeywordRepository.java
+++
@@ -1,7 +0,0 @@
-package com.bearindonesia.repository;
-
-import com.bearindonesia.domain.Keyword;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface KeywordRepository extends JpaRepository<Keyword, Long> {}
-
